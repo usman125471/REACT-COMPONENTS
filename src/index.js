f8e9165fa08605d8c464dedcca62a6572e2a4034
usman125471 <usman.125471@email.com>
2022-04-27 12:42:06 +0500

    First Code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import CreateRoot from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\
+
+function Greetings() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+
+const Person = () => <h1>Usman!</h1>;
+const Message = () => (
+  <h2>
+    <p>learning react. </p>
+  </h2>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\
+
+// function Greetings() {
+//   return (
+//     <div>
+//       <h3>Usman! here and that's my first component.</h3>
+//       <ul>
+//         <li>
+//           <a href='#'>About</a>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+////////////////////////\\\\\\\\\\\\\\\\\\\\\
+// function Greetings() {
+//   return <div>
+//     <h1>Usman!</h1>
+//   </div>
+// }
+
+//////////////////////\\\\\\\\\\\\\\\\\\\\
+
+// const Greetings = () => {
+//   return React.createElement('h1', {},'Myself Usman! Learning React.')
+//  }
+
+////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\
+// const Greetings = () => {
+//   return React.createElement("div", {}, React.createElement('h1',{},'Web Devolper!' ));
+//  }
+
+CreateRoot.render(<Greetings />, document.getElementById("root"));
